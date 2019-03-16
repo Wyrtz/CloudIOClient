7d4d402739b5d95b5928280737978e48ce621fca
Sofus Würtz <sofuspw@gmail.com>
2019-03-16 11:42:48 +0100

    Made the generators randomness crypto safe with secrets

--- pin_generator.py
+++ pin_generator.py
@@ -1,27 +1,26 @@
+import secrets
 import string
-import random
-# Todo: Random not crypto safe!
+
 
 class pin_gen:
 
     def __init__(self, length):
         self.length = length
-        self.pin = None
+        self.pin = None # Todo: Hash the pin ?
 
     def generate_pin(self, alphabet=True):
-        pin = ""
         dict = string.digits
         if alphabet:
             dict += string.ascii_uppercase
-        for i in range(self.length):
-            pin += str(dict[random.randint(-1, len(dict)-1)])
-        self.pin = pin
-        return pin
+        secGen = secrets.SystemRandom()
+        self.pin = "".join(secGen.sample(dict, self.length))
+        return self.pin
 
     def verify_pin(self, pin):
-        return pin == self.pin
+        return secrets.compare_digest(pin, self.pin)
 
 gen = pin_gen(4)
-pin = gen.generate_pin(alphabet=False)
+pin = gen.generate_pin(alphabet=True)
 print(pin)
 print(gen.verify_pin(pin))
+print(gen.pin) # ToDo: is this a problem ?
