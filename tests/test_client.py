db1208807c3c87665b00ebfe08e0fdaedde8817d
Sofus Würtz <sofuspw@gmail.com>
2019-04-29 15:58:52 +0200

    Made sure test_client also pass

--- tests/test_client.py
+++ tests/test_client.py
@@ -13,14 +13,11 @@ from security import keyderivation
 
 class TestClient(unittest.TestCase):
 
-    def register_user(self, userID):
-        requests.post('https://' + self.serverIp + '/register/' + userID, verify=False)
-
     def unregister_user(self, userID):
         requests.post('https://' + self.serverIp + '/unregister/' + userID, verify=False)
 
     def setUp(self):
-        self.serverIp = '127.0.0.1:443' # 'wyrnas.myqnapcloud.com:8000'
+        self.serverIp = 'wyrnas.myqnapcloud.com:8001' #'127.0.0.1:443'
         self.sleep_time = 1
         self.random_files_list = []
         self.username = "abecattemattemad"
@@ -29,7 +26,6 @@ class TestClient(unittest.TestCase):
         self.ste = ste.global_test_configer(self.kd)
         self.enc = self.kd.select_first_pw(self.pw)
         self.client = Client(username=self.username, password=self.pw, server_location=self.serverIp)
-        self.register_user(self.client.userID)
 
     def tearDown(self):
         self.ste.recover_resources()
@@ -45,7 +41,7 @@ class TestClient(unittest.TestCase):
         """Create a new random file and place it in files folder for upload"""
         random_file_path = self.create_random_file().relative_to(globals.WORK_DIR)
         sleep(self.sleep_time)
-        file_list_enc = self.client.servercoms.get_file_list(self.client.userID)
+        file_list_enc = self.client.servercoms.get_file_list()
         file_list_dec = self.client.file_crypt.decrypt_file_list(file_list_enc)
         self.assertIn(random_file_path, file_list_dec)
 
@@ -58,7 +54,7 @@ class TestClient(unittest.TestCase):
         self.random_files_list.remove(random_file_path)
         sleep(self.sleep_time)
         self.assertNotIn(random_file_relative_path, self.client.get_local_file_list())
-        file_list_enc = self.client.servercoms.get_file_list(self.client.userID)
+        file_list_enc = self.client.servercoms.get_file_list()
         file_list_dec = self.client.file_crypt.decrypt_file_list(file_list_enc)
         self.assertNotIn(random_file_relative_path, file_list_dec)
 
@@ -69,7 +65,7 @@ class TestClient(unittest.TestCase):
         random_file_relative_path = random_file_path.relative_to(globals.WORK_DIR)
         random_file_name = pl.Path(random_file_path.name)
         sleep(self.sleep_time)
-        file_list_enc = self.client.servercoms.get_file_list(self.client.userID)
+        file_list_enc = self.client.servercoms.get_file_list()
         file_list_dec = self.client.file_crypt.decrypt_file_list(file_list_enc)
         self.assertIn(random_file_relative_path, file_list_dec)
         # Delete the file locally, "close client" such that the server is not asked to also delete (archive)
